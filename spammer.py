e3b654d5ae680a757f5f8fdbc7b09260370fa8ec
cosmic <130719744+cosmicmonkey659@users.noreply.github.com>
2024-04-19 21:48:40 +0100

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -1,15 +1,1 @@
-from dhooks import Webhook, file
-import time
-print(""""
-█▀▄▀█ ▄▀█ █▀▄ █▀▀     █▄▄ █▄█     ░░█ █▄█ █▄█ █▄█ █▄█     █▀█ █▄░█     █▀▄ █ █▀ █▀▀ █▀█ █▀█ █▀▄
-█░▀░█ █▀█ █▄▀ ██▄     █▄█ ░█░     █▄█ ░█░ ░█░ ░█░ ░█░     █▄█ █░▀█     █▄▀ █ ▄█ █▄▄ █▄█ █▀▄ █▄▀
-      """)
 
-message = input("What do you want to spam: ")
-webhook = Webhook(input("what the webhook: "))
-delay = int(input("Enter delay: "))
-
-while True:
-    time.sleep(delay)
-    webhook.send(message)
-    print("Send.")
